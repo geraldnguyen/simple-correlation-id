073b1f30d9626935b991a9f94089743552bd2c1a
Gerald Nguyen <ngthehuy@gmail.com>
2022-10-27 23:10:33 +0800

    advanced options

--- index.js
+++ index.js
@@ -5,15 +5,44 @@ const correlationId = function (system, prettyUniqueIdGenerator) {
   // default to timestamp
   prettyUniqueIdGenerator = prettyUniqueIdGenerator || timestamp;
 
-  let ci = `${system}-${prettyUniqueIdGenerator()}`;
+  // advance options
+  if (typeof prettyUniqueIdGenerator === 'object' ) {
+    prettyUniqueIdGenerator = advanceIdGenerator(prettyUniqueIdGenerator);
+  }
 
-  return ci.replace(/^-/, '');    // remove prefix - caused by empty system
+  const ci = `${system}-${prettyUniqueIdGenerator()}`;
+
+  return removeEmptyDashPrefix(ci);
 };
 
+const advanceIdGenerator = (advanceOptions) => {
+  let { sessionIdInjector = empty, idGenerator, randSize = 4 } = advanceOptions;
+  
+  if (idGenerator == null) {
+    idGenerator = () => {
+      const random = Math.random() * Math.pow(10, randSize);
+      return `${timestamp()}-${Math.floor(random)}`;
+    }
+  }
 
+  return () => {
+    const id = `${sessionIdInjector()}-${idGenerator()}`;
+
+    return removeEmptyDashPrefix(id);
+  };
+  
+}
+
+function empty() {
+  return '';
+}
 
 function timestamp() {
   return Date.now();
+}
+
+function removeEmptyDashPrefix(ci) {
+  return ci.replace(/^-/, '');  // remove prefix - caused by empty system
 }
 
 module.exports = correlationId;
--- index.test.js
+++ index.test.js
@@ -24,6 +24,28 @@ test('prefix source system if supply', () => {
   expect(parseInt(timestampPart)).toBeLessThanOrEqual(afterTs);  
 });
 
-test('use the id generator', () => {
+test('pass the id generator as a function', () => {
   expect(correlationId('test', () => 'id')).toBe('test-id');
+});
+
+test('pass the id generator an option', () => {
+  const options = {
+    idGenerator: () => 'id'
+  };
+  expect(correlationId('test', options)).toBe('test-id');
+});
+
+test('advance options: default values', () => {
+  const options = { };
+  expect(correlationId('test', options)).toEqual(expect.stringMatching(/^test-\d+-\d{4}$/));
+});
+
+test('advance options: override randSize', () => {
+  const options = { randSize: 5 };
+  expect(correlationId('test', options)).toEqual(expect.stringMatching(/^test-\d+-\d{5}$/));
+});
+
+test('advance options: override sessionId', () => {
+  const options = { sessionIdInjector: () => 'session' };
+  expect(correlationId('test', options)).toEqual(expect.stringMatching(/^test-session-\d+-\d{4}$/));
 });
